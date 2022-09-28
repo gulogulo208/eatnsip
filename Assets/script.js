ea3ef103474e8f2737b31f37b08f1d8c1657dedf
Theodore Elgee <elgee.t@gmail.com>
2022-09-27 20:43:41 -0700

    committing team-reviewed readme, script.js with tested functionality, and index.html with rearranged elements

---
+++ Assets/script.js
@@ -0,0 +1,137 @@
+const FoodsrchBtn = document.getElementById('food-btn');
+const DrinksrchBtn = document.getElementById('drink-btn');
+const recipeList = document.getElementById('recipe');
+const recipeDetailsContent = document.querySelector('.recipe-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
+
+// event listeners
+FoodsrchBtn.addEventListener('click', foodrecipeList);
+DrinksrchBtn.addEventListener('click', drinkrecipeList);
+recipeList.addEventListener('click', getRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    recipeDetailsContent.parentElement.classList.remove('showRecipe');
+});
+
+
+// get meal list that matches with the ingredients
+function foodrecipeList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "food-item" data-id = "${meal.idMeal}">
+                        <div class = "food-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "food-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "food-recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            recipeList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            recipeList.classList.add('notFound');
+        }
+
+        recipeList.innerHTML = html;
+    });
+}
+
+// Drink Java Section
+
+function drinkrecipeList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.drinks){
+            data.drinks.forEach(drink => {
+                html += `
+                    <div class = "food-item" data-id = "${drink.idDrink}">
+                        <div class = "food-img">
+                            <img src = "${drink.strDrinkThumb}" alt = "food">
+                        </div>
+                        <div class = "food-name">
+                            <h3>${drink.strDrink}</h3>
+                            <a href = "#" class = "drink-recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            recipeList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            recipeList.classList.add('notFound');
+        }
+
+        recipeList.innerHTML = html;
+    });
+}
+
+// get recipe of the meal
+function getRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('food-recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => foodRecipeModal(data.meals));
+    } else if(e.target.classList.contains('drink-recipe-btn')){
+        let drinkItem = e.target.parentElement.parentElement;
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => drinkRecipeModal(data.drinks));
+    }
+}
+
+// create a modal
+function foodRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    recipeDetailsContent.innerHTML = html;
+    recipeDetailsContent.parentElement.classList.add('showRecipe');
+}
+
+
+// create a modal
+function drinkRecipeModal(drink){
+    drink = drink[0];
+    let html = `
+        <h2 class = "recipe-title">${drink.strDrink}</h2>
+        <p class = "recipe-category">${drink.strDrink}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${drink.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${drink.strDrinkThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${drink.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    recipeDetailsContent.innerHTML = html;
+    recipeDetailsContent.parentElement.classList.add('showRecipe');
+}
--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # eatNsip
 
-This is a food and cocktail recipe search app that will take keywords and return recipes containing the search terms.
+Mobile and Web App That Helps You Find Cocktail and Food Recipes for Leftover and Unused Food
 
-## User Story
+## Description
 
+<!-- What was your motivation? -->
+The main motivation behind this project was to create a web application for people who have leftover food in their house. Most youngsters like us have leftovers in their house and unfortunately is often later thrown away because people normally don't have ideas on what to do with those leftovers, so we thought of creating a web application to not let those ingredients go to waste and make people more creative.
 
+<!-- Why did you build this project? -->
+Leftovers and surplus food are often thrown out after parties or events and this app provides a way to quickly utilize unused food and alcohol.
 
+<!-- What problem does it solve? -->
+Users will be able to scrape large databases of food and drink recipes with a simple search rather than Googling each individual recipe or drink options to give user ideas.
+This will reduce food waste and give the user a list of ideas to create something with random food items they have lying around the pantry as well as cocktail recipes for leftover alcohol. In short this app tackles food waste and decreases search time for recipes. 
 
+<!-- What did you learn? -->
+Team work is what we mainly learned from this project. Separating tasks makes our work easier compared to using a single brain. Giving feedback and receiving helps to improve on your work from your peers.
+JavaScript is what we mostly struggle with as we need to use logic on it. With the help of this project it gave us a challange to think beyond what you are capable of. We were able to use third party APIs to simplify the functionality and request ==> response portion of our application. We did significant research into both the APIs and the JavaScript logic when we encountered bugs and code that failed to execute. 
+Applying what we learned in our class in this project was the most important thing. Rather than just consuming knowledge it makes it more fruitful if we can actually apply this knowledge in real life situations and that was what we were capable of doing in this project.
 
-## User Acceptance Testing
-
-
-
-<!-- Image example of app -->
+# Screen-shot
+<!-- insert image folder name in (/relative/path/images/img.png)  -->
+![alt text](/relative/path/images/img.png)
+![alt text](/relative/path/images/img.png)
